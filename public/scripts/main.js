ce2adea5ed32224400e44a567d7f0752c228a47a
ponyspy <desade4me@gmail.com>
2014-01-13 19:17:08 +0400

    main page

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -2,7 +2,9 @@ $(document).ready(function() {
 	var count = 0;
 
 	$('.menu_block, .content_block').click(function(event) {
+		$('.work_item').hide();
 		$('.background_item').eq(count+2).css('opacity', '0');
+		$('.work_item').eq(count+2).show();
 		count--;
 		if (count == -3) {
 			$('.background_item').css('opacity', '1')
