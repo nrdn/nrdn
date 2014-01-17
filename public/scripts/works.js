1bad1c9b7a891fe0d05f8d8b60b416bcae3fdaba
ponyspy <desade4me@gmail.com>
2014-01-17 18:55:16 +0400

    styles

--- public/scripts/works.js
+++ public/scripts/works.js
@@ -1,6 +1,12 @@
 $(document).ready(function() {
-	$('.work_item').mouseover(function(event) {
-		$(this).css('color', 'white');
-		$(this).children('.work_inner').removeAttr('style');
-	});
+	$('.work_item').on({
+		mouseover: function() {
+			$(this).css('color', 'white');
+			$(this).children('.work_inner').css('background-image', 'url(/images/design/logo2.png)').css('background-size', '35%');
+		},
+		mouseout: function() {
+			$(this).css('color', 'black');
+			$(this).children('.work_inner').css('background-image', 'url(/images/design/logo4.png)').css('background-size', '150%');
+		}
+	})
 });
