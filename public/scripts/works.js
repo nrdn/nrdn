ee9d9d0e33b52158783a88a4eef71ca8a65103b9
ponyspy <desade4me@gmail.com>
2014-01-21 18:01:05 +0400

    works

--- public/scripts/works.js
+++ public/scripts/works.js
@@ -1,23 +1,28 @@
 function getRandom (min, max) {
-	var rand = min - 0.5 + Math.random()*(max-min+1)
+	var rand = min - 0.5 + Math.random()*(max-min)
 	return Math.round(rand);
 }
 
 $(document).ready(function() {
 	$('.work_item').on({
 		mouseover: function() {
-			$(this).children('a, .work_title').hide();
+			$(this).children('.work_logo, .work_title').hide();
 			$(this).children('.work_images').show();
 		},
 		mouseout: function() {
-			$(this).children('a, .work_title').show();
+			$(this).children('.work_logo, .work_title').show();
 			$(this).children('.work_images').hide();
 		},
-		mousemove: function() {
+		mousemove: function(event) {
+			var msg = 0;
+			var msg = event.pageX + event.pageY;
+
 			var max = $(this).find('.work_image').length;
-			var rand = getRandom(0, max);
-			$(this).find('.work_image').hide();
-			$(this).find('.work_image').eq(rand).show();
+			if (max > 1) {
+				var rand = getRandom(0, max);
+				$(this).find('.work_image').hide();
+				$(this).find('.work_image').eq(rand).show();
+			}
 		}
 	});
 });
