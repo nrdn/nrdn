ff9f21a11a3b6f15f6d95e6a60e6834db8037d3d
ponyspy <desade4me@gmail.com>
2014-01-24 17:30:04 +0400

    contacts

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function() {
 	var count = $('.background_item').length - 1;
+	var w_count = $('.work_item').length - 1;
 
 	$('.menu_block, .content_block').click(function(event) {
 		$('.work_item').hide();
-		$('.background_item').eq(count).css('opacity', '0');
 		$('.work_item').eq(count).show();
+		$('.background_item').eq(count).css('opacity', '0');
 		count--;
 		if (count < 0) {
 			$('.background_item').css('opacity', '1')
