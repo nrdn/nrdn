ece04411c3aa4a2d523d767a3299dfce9f3c6f66
ponyspy <desade4me@gmail.com>
2014-01-27 17:00:24 +0400

    index works

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,15 +1,20 @@
 $(document).ready(function() {
 	var count = $('.background_item').length - 1;
-	var w_count = $('.work_item').length - 1;
+	var w_count = 0;
 
-	$('.menu_block, .content_block').click(function(event) {
+	$('.content_block').click(function(event) {
+
+		w_count++;
 		$('.work_item').hide();
-		$('.work_item').eq(count).show();
+		$('.work_item').eq(w_count).show();
 		$('.background_item').eq(count).css('opacity', '0');
 		count--;
+
 		if (count < 0) {
 			$('.background_item').css('opacity', '1')
 			count = $('.background_item').length - 1;
+			w_count = 0;
+			$('.work_item').eq(w_count).show();
 		}
 	});
 });
