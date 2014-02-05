0f7e6fc36baf0f81476a60b51c25b9f8f801ab80
ponyspy <desade4me@gmail.com>
2014-02-05 19:27:17 +0400

    link

--- public/scripts/blog.js
+++ public/scripts/blog.js
@@ -14,11 +14,12 @@ $(document).ready(function() {
 					$.each(posts, function(index, post) {
 						var post_item = $('<div />', {'class':'post_item_block'});
 						var post_img = $('<img />', {'class':'post_img', 'src': post.image});
+						var post_img_link = $('<a />', {'class':'post_title', 'href': '/blog/' + post.b_id});
 						var post_dscription = $('<div />', {'class':'post_dscription_block'});
 						var post_title = $('<a />', {'class':'post_title', 'href': '/blog/' + post.b_id, 'text': post.ru.title});
 						var post_body = $('<div />', {'class':'post_body', 'html': post.ru.body});
 
-						$('.content_block').append(post_item.append(post_title.append(post_img), post_dscription.append(post_title), post_body));
+						$('.content_block').append(post_item.append(post_img_link.append(post_img), post_dscription.append(post_title), post_body));
 					});
 				}
 				else {
