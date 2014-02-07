d9fbbe94a204d163a90d6ef162370501a945a02f
ponyspy <desade4me@gmail.com>
2014-02-07 18:46:36 +0400

    new map

--- public/scripts/g_map.js
+++ public/scripts/g_map.js
@@ -1,11 +1,28 @@
 var map;
 function initialize() {
+	var myLatlng = new google.maps.LatLng(55.7638563, 37.6131137);
+
   var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
+    zoom: 18,
+    center: myLatlng
   };
+
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
+
+	var infowindow = new google.maps.InfoWindow({
+	    content: 'Народный архитектор'
+	});
+
+	var marker = new google.maps.Marker({
+	    position: myLatlng,
+	    map: map,
+	    title: 'Народный архитектор'
+	});
+
+	google.maps.event.addListener(marker, 'click', function() {
+	    infowindow.open(map, marker);
+	});
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
