f611e64434062ae3ca48dbbbd667efa7d193935b
ponyspy <desade4me@gmail.com>
2014-02-10 19:58:52 +0400

    random index works

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,21 +1,32 @@
+function getRandom (min, max) {
+	var rand = min - 0.5 + Math.random()*(max-min)
+	return Math.round(rand);
+}
+
 $(document).ready(function() {
 	var count = $('.background_item').length - 1;
-	var w_count = 0;
+	var w_count = getRandom(0, count);
+	var old_rand = w_count;
+
+	$('.work_item').eq(w_count).show();
+	$('.background_item').eq(w_count).css('opacity', '1');
 
 	$(document).click(function(event) {
+		w_count = getRandom(0, count);
 
-		w_count++;
+		if (w_count == old_rand) {
+			w_count++;
+			if (w_count > count) w_count = 0;
+				old_rand = w_count;
+		}
+		else {
+			old_rand = w_count;
+		}
+
 		$('.work_item').hide();
 		$('.work_item').eq(w_count).show();
-		$('.background_item').eq(count).css('opacity', '0');
-		$('.background_item').eq(count-1).css('opacity', '1');
-		count--;
+		$('.background_item').css('opacity', '0');
+		$('.background_item').eq(w_count).css('opacity', '1');
 
-		if (count < 0) {
-			$('.background_item').css('opacity', '1')
-			count = $('.background_item').length - 1;
-			w_count = 0;
-			$('.work_item').eq(w_count).show();
-		}
 	});
 });
