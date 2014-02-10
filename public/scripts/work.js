9d0ae88088557d0e20aac00f00bdd733d6d71c85
ponyspy <desade4me@gmail.com>
2014-02-10 20:28:16 +0400

    work target

--- public/scripts/work.js
+++ public/scripts/work.js
@@ -7,18 +7,22 @@ $(document).ready(function() {
 	});
 
 	$(document).click(function(event) {
-		w_count++;
-		$('.work_item').hide();
-		$('.work_item').eq(w_count).show();
-		$('.background_item').eq(count).css('opacity', '0');
-		$('.background_item').eq(count-1).css('opacity', '1');
-		count--;
-
-		if (count < 0) {
-			$('.background_item').css('opacity', '1')
-			count = $('.background_item').length - 1;
-			w_count = 0;
+		var target = $( event.target );
+		console.log(target)
+		if (target.is( ':not(.logo, .menu_items a)' )) {
+			w_count++;
+			$('.work_item').hide();
 			$('.work_item').eq(w_count).show();
+			$('.background_item').eq(count).css('opacity', '0');
+			$('.background_item').eq(count-1).css('opacity', '1');
+			count--;
+
+			if (count < 0) {
+				$('.background_item').css('opacity', '1')
+				count = $('.background_item').length - 1;
+				w_count = 0;
+				$('.work_item').eq(w_count).show();
+			}
 		}
 	});
 });
