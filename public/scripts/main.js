45487b2775c5a10fefb2f3fe28ddeb3446501aec
ponyspy <desade4me@gmail.com>
2014-02-10 20:22:43 +0400

    index target

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -12,21 +12,23 @@ $(document).ready(function() {
 	$('.background_item').eq(w_count).css('opacity', '1');
 
 	$(document).click(function(event) {
-		w_count = getRandom(0, count);
+		var target = $( event.target );
+		if (target.is( ':not(.logo, .menu_items a)' )) {
+			w_count = getRandom(0, count);
 
-		if (w_count == old_rand) {
-			w_count++;
-			if (w_count > count) w_count = 0;
+			if (w_count == old_rand) {
+				w_count++;
+				if (w_count > count) w_count = 0;
+					old_rand = w_count;
+			}
+			else {
 				old_rand = w_count;
-		}
-		else {
-			old_rand = w_count;
-		}
+			}
 
-		$('.work_item').hide();
-		$('.work_item').eq(w_count).show();
-		$('.background_item').css('opacity', '0');
-		$('.background_item').eq(w_count).css('opacity', '1');
-
+			$('.work_item').hide();
+			$('.work_item').eq(w_count).show();
+			$('.background_item').css('opacity', '0');
+			$('.background_item').eq(w_count).css('opacity', '1');
+		}
 	});
 });
