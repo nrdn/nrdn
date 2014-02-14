cd02c4b4b35ef9079620d9e8d8c85d14cbce73d7
ponyspy <desade4me@gmail.com>
2014-02-14 14:43:34 +0400

    work logic

--- public/scripts/work.js
+++ public/scripts/work.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+	$('.dot:first').text('●');
 	var count = $('.background_item').length - 1;
 	var w_count = 0;
 
@@ -6,46 +7,59 @@ $(document).ready(function() {
 		$('.background_works').toggleClass('glow');
 	}
 
-	$('.work_description_block').on('mouseover mouseout', toggleGlow);
+	function hideDescription () {
+		$('.work_description_block').data('clicked', true);
+		$('.work_description_block').addClass('hide');
+		$('.work_description_block').children().hide();
+		$('.work_description_block').off('mouseover mouseout');
+		$('.background_works').addClass('glow');
+		$(document).off('click', hideDescription);
+	}
 
-	$('.work_description_block').click(function(event) {
-		$(this).data('clicked', !$(this).data('clicked'));
-		$(this).toggleClass('hide');
+	function toggleDescription () {
+		$('.work_description_block').data('clicked', !$('.work_description_block').data('clicked'));
+		$('.work_description_block').toggleClass('hide');
 
-		if ($(this).data('clicked')) {
-			$(this).children().hide();
-			$(this).off('mouseover mouseout')
+
+		if ($('.work_description_block').data('clicked')) {
+			$('.work_description_block').children().hide();
+			$('.work_description_block').off('mouseover mouseout')
 			$('.background_works').addClass('glow');
 		}
 		else {
-			$(this).children().show();
-			$(this).on('mouseover mouseout', toggleGlow);
+			$('.work_description_block').children().show();
+			$('.work_description_block').on('mouseover mouseout', toggleGlow);
 		}
-	});
+	}
 
-	$('.dot:first').text('●');
+	function backScroll(event) {
+			var target = $(event.target);
+			$('.round_click_block').hide();
+			$('.background_works').addClass('glow');
 
-	$(document).click(function(event) {
-		var target = $(event.target);
-		$('.round_click_block').hide();
-
-		if (target.is( ':not(.logo, .menu_items a, .work_description_block, .work_title, .work_description)' )) {
-			w_count++;
-			$('.work_item').hide();
-			$('.work_item').eq(w_count).show();
-			$('.dot').text('◦');
-			$('.dot').eq(w_count).text('●');
-			$('.background_item').eq(count).css('opacity', '0');
-			$('.background_item').eq(count-1).css('opacity', '1');
-			count--;
-
-			if (count < 0) {
-				$('.background_item').css('opacity', '1')
-				count = $('.background_item').length - 1;
-				w_count = 0;
+			if (target.is( ':not(.logo, .menu_items a)' )) {
+				w_count++;
+				$('.work_item').hide();
 				$('.work_item').eq(w_count).show();
+				$('.dot').text('◦');
 				$('.dot').eq(w_count).text('●');
+				$('.background_item').eq(count).css('opacity', '0');
+				$('.background_item').eq(count-1).css('opacity', '1');
+				count--;
+
+				if (count < 0) {
+					$('.background_item').css('opacity', '1')
+					count = $('.background_item').length - 1;
+					w_count = 0;
+					$('.work_item').eq(w_count).show();
+					$('.dot').eq(w_count).text('●');
+				}
 			}
 		}
-	});
+
+	// $('.work_description_block').on('mouseover mouseout', toggleGlow);
+
+	$('.work_description_block').on('click', toggleDescription);
+	$(document).on('click', hideDescription);
+	$(document).on('click', backScroll);
 });
